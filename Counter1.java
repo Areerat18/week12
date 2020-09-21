12e0dde65d38104b60f75dcbb97a372be4f793f7
MissAreerat Rattanamun <50565493+Areerat18@users.noreply.github.com>
2020-09-21 16:35:16 +0700

    Add files via upload

---
+++ Counter1.java
@@ -0,0 +1,10 @@
+package week12;
+
+class Counter1{
+	int count = 0;
+	
+	Counter1(){
+		count++;
+		System.out.println(count);
+	}
+}
---
+++ TestStudent.java
@@ -0,0 +1,8 @@
+package week12;
+
+public class TestStudent {
+	public static void main(String[]args) {
+		Student1 std1 = new Student1(390100,"Jame Smit");
+		std1.display();
+	}
+}
